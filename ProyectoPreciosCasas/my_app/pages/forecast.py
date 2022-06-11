5d935a0a962139d5addef57f43581996bb4e4e5c
sebmatecho <slozanof@unal.edu.co>
2022-06-11 13:41:46 -0500

    modelo actualizado

--- ProyectoPreciosCasas/my_app/pages/forecast.py
+++ ProyectoPreciosCasas/my_app/pages/forecast.py
@@ -9,7 +9,7 @@ import tempfile
 
 st.set_page_config(page_title='App - Pronóstico',
                     layout="wide", 
-                    page_icon='fa-solid fa-chart-line',  
+                    page_icon='🚀',  
                     initial_sidebar_state="expanded")
 
 st.title("Pronosticando precios de casas")
